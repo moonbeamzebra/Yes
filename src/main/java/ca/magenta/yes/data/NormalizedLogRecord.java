bce83799ba6222b8d28749a7a954e267f5b9e6f2
jplaberge <moonbeamzebra@magenta.ca>
2017-01-08 17:43:31 -0500

    new NormalizedLogRecord class

--- src/main/java/ca/magenta/yes/data/NormalizedLogRecord.java
+++ src/main/java/ca/magenta/yes/data/NormalizedLogRecord.java
@@ -3,162 +3,151 @@ package ca.magenta.yes.data;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexableField;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jplaberge on 2016-12-31.
  */
 public class NormalizedLogRecord {
 
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass().getPackage().getName());
+
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz");
 
-    private long srcTimestamp;
-    private long rxTimestamp;
+    private final HashMap<String, Object> data;
 
-    private String deviceName;
-    private String partition;
-    private String source;
-    private String dest;
-    private String port;
-    private String action;
-    private String description;
-
-    private String type;
-
-    private String message;
+//    private long srcTimestamp;
+//    private long rxTimestamp;
+//
+//    private String deviceName;
+//    private String partition;
+//    private String source;
+//    private String dest;
+//    private String port;
+//    private String action;
+//    private String description;
+//
+//    private String type;
+//
+//    private String message;
 
     public NormalizedLogRecord(Document logRecordDoc) {
-        this.srcTimestamp = Long.valueOf(logRecordDoc.get("txTimestamp"));
-        this.rxTimestamp = Long.valueOf(logRecordDoc.get("rxTimestamp"));
-        this.deviceName = logRecordDoc.get("device");
-        this.partition = logRecordDoc.get("partition");
-        this.source = logRecordDoc.get("source");
-        this.dest = logRecordDoc.get("dest");
-        this.port = logRecordDoc.get("port");
-        this.action = logRecordDoc.get("action");
-        this.description = logRecordDoc.get("msg");
-        this.type = logRecordDoc.get("type");
 
-        this.message = logRecordDoc.get("message");
+        data = new HashMap<String, Object>();
+
+        for (IndexableField field : logRecordDoc.getFields()) {
+            logger.info(String.format("field:[%s]; [%s]", field.name(), field.fieldType().toString()));
+            switch (field.name()) {
+                case "txTimestamp":
+                case "rxTimestamp":
+                    data.put(field.name(), Long.valueOf(logRecordDoc.get(field.name())));
+                    break;
+                default:
+                    data.put(field.name(), logRecordDoc.get(field.name()));
+            }
+        }
+
     }
 
     public NormalizedLogRecord() {
+        data = new HashMap<String, Object>();
+    }
+
+    public NormalizedLogRecord(HashMap<String, Object> data) {
+        this.data = data;
     }
 
     @Override
     public String toString() {
-        return "{" +
-                "srcTimestamp=" + srcTimestamp +
-                ", rxTimestamp=" + rxTimestamp +
-                ", deviceName='" + deviceName + '\'' +
-                ", partition='" + partition + '\'' +
-                ", source='" + source + '\'' +
-                ", dest='" + dest + '\'' +
-                ", port='" + port + '\'' +
-                ", action='" + action + '\'' +
-                ", description='" + description + '\'' +
-                ", type='" + type + '\'' +
-                ", message='" + message + '\'' +
-                '}';
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append('{');
+
+        int n = 0;
+        for (Map.Entry<String, Object> entry : data.entrySet())
+        {
+            if (n!=0) sb.append(',');
+            n++;
+            sb.append(entry.getKey());
+            sb.append('=');
+            sb.append(entry.getKey().toString());
+        }
+
+        sb.append("}");
+
+        return sb.toString();
     }
 
     public String toTTYString(boolean withOriginalMessage, boolean oneLiner) {
 
+        StringBuilder sb = new StringBuilder();
 
-        String ttyString = String.format("%s" +
-                        ", deviceName='" + deviceName + '\'' +
-                        ", partition='" + partition + '\'' +
-                        ", source='" + source + '\'' +
-                        ", dest='" + dest + '\'' +
-                        ", port='" + port + '\'' +
-                        ", action='" + action + '\'' +
-                        ", description='" + description + '\'' +
-                        ", type='" + type + "'",
-                prettyRxTimestamp());
+        sb.append(prettyRxTimestamp());
+
+        for (Map.Entry<String, Object> entry : data.entrySet())
+        {
+            if ( ! "rxTimestamp".equals(entry.getKey())) {
+                sb.append(',');
+                sb.append(entry.getKey().toString());
+                sb.append("='");
+                sb.append(entry.getValue());
+                sb.append("'");
+            }
+        }
 
         if (withOriginalMessage)
         {
             if (oneLiner)
             {
-                ttyString = ttyString + ", message='" + message + "'";
+                sb.append( ", message='" + getMessage() + "'");
             }
             else
             {
-                ttyString = ttyString + "\n  " + message;
+                sb.append( "\n  " + getMessage());
             }
         }
 
-        return ttyString;
+        return sb.toString();
     }
 
     public String toJson() throws JsonProcessingException {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        return  mapper.writeValueAsString(this);
+        return  mapper.writeValueAsString(data);
     }
 
     public static NormalizedLogRecord fromJson(String jSon) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        return mapper.readValue(jSon, NormalizedLogRecord.class);
+        HashMap<String, Object> data = mapper.readValue(jSon, HashMap.class);
 
-    }
+        return new NormalizedLogRecord(data);
 
-
-    public long getSrcTimestamp() {
-        return srcTimestamp;
-    }
-
-    public String prettySrcTimestamp() {
-        return DATE_FORMAT.format(srcTimestamp);
     }
 
     public long getRxTimestamp() {
-        return rxTimestamp;
+        return (long) data.get("rxTimestamp");
     }
 
     public String prettyRxTimestamp() {
-        return DATE_FORMAT.format(rxTimestamp);
-    }
-
-    public String getDeviceName() {
-        return deviceName;
+        return DATE_FORMAT.format(getRxTimestamp());
     }
 
     public String getPartition() {
-        return partition;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public String getDest() {
-        return dest;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getType() {
-        return type;
+        return data.get("partition").toString();
     }
 
     public String getMessage() {
-        return message;
+        return data.get("message").toString();
     }
 
 }
